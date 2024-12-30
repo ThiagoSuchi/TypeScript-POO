a344aa97136deccf6ee0173d4e0e7479002d9c14
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-12-30 14:56:15 -0400

    Classes, Métodos, Atributos abistratos...

---
+++ src/Abstratos.ts
@@ -0,0 +1,49 @@
+// Classe abstract: Define uma base comum para outras classes, mas não pode ser usada diretamente.
+// Método abstract: Força as classes derivadas a fornecerem uma implementação específica.
+
+// Super Classe
+export abstract class Personagem {
+    protected abstract emoji: string;
+
+    constructor(
+        protected nome: string,
+        protected ataque: number,
+        protected vida: number,
+    ) {}
+
+    atacar(personagem: Personagem): void {
+        this.bordao()
+        personagem.perderVida(this.ataque);
+    }
+
+    perderVida(forcaAtaque: number): void {
+        this.vida -= forcaAtaque
+        console.log(`${this.nome} agora tem ${this.vida} de vida...`);
+    }
+
+    abstract bordao(): void;
+}
+
+// Subclasses
+export class Guerreira extends Personagem {
+    protected emoji = '\u{1F9DD}';
+    bordao(): void {
+        console.log( this.emoji + ' AAAH VOCÊ IRÀ MORRER CRIATURA DEMONIACA!');
+    }
+};
+
+export class Monstro extends Personagem {
+    protected emoji = '\u{1F9DF}';
+    bordao(): void {
+        console.log( this.emoji + ' HAHAHAhaha, eu irei te destruir ser insignificante.');
+    }
+};
+
+const guerreira = new Guerreira('Guerreira', 100, 1000);
+const monstro = new Monstro('Monstro', 200, 800);
+
+guerreira.atacar(monstro);
+monstro.atacar(guerreira);
+monstro.atacar(guerreira);
+guerreira.atacar(monstro);
+guerreira.atacar(monstro);
